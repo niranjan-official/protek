43a617b024701a9ae59f98cfd1089b30459a2732
vineethkv7736 <vineethkv7736@gmail.com>
2024-09-27 23:09:29 +0530

    auth ui done

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,32 +3,54 @@ import { login } from "../actions";
 
 const page = () => {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-slate-50">
-      <form
-        className="flex w-fit flex-col gap-4 rounded-xl border p-4"
-        action={login}
-      >
-        <h2 className="text-2xl font-bold">SignIn</h2>
-        <hr className="w-full" />
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-slate-100 px-4">
+    <form
+      className="flex w-full flex-col gap-4 rounded-xl border-2 border-black/50 bg-white p-4 shadow-xl md:w-80"
+      action={login}
+    >
+      <div className="flex flex-col items-center justify-end">
+        <p className="text-2xl font-serif">Login In To</p>
+        <h1 className="font-sans text-4xl font-semibold text-black/80">
+          PROTEK 2024
+        </h1>
+        <p className="text-sm text-black/50">Powered by μLearn PRC</p>
+        <hr className="mt-3 w-3/4 bg-black/10" />
+      </div>
 
-        <label htmlFor="email">
-          Email
-          <input type="email" name="email" id="email" />
-        </label>
+      <label htmlFor="email">
+        <h1 className="w-max rounded-xl bg-rose-200 p-2 font-serif text-sm">
+          E Mail
+        </h1>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="example@gmail.com"
+          className="border-2 border-black/30"
+        />
+      </label>
 
-        <label htmlFor="password">
+      <label htmlFor="password">
+        <h1 className="w-max rounded-xl bg-purple-300 p-2 font-serif text-sm">
           Password
-          <input type="password" name="password" id="password" />
-        </label>
+        </h1>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+          className="border-2 border-black/30"
+        />
+      </label>
 
-        <button
-          type="submit"
-          className="rounded-[0.5rem] bg-blue-500 p-2 text-white"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
+      <button
+        type="submit"
+        className="rounded-[0.5rem] bg-green-500 p-2 font-serif"
+      >
+        Submit
+      </button>
+    </form>
+  </div>
   );
 };
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -2,53 +2,76 @@ import React from "react";
 import { signup } from "../actions";
 
 const page = () => {
-
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-slate-50">
+    <div className="m-h-screen flex w-full flex-col items-center justify-center bg-slate-50 p-4">
       <form
-        className="flex w-fit flex-col gap-4 rounded-xl border p-4"
+        className="flex w-full flex-col gap-4 rounded-xl border-2 border-black/50 bg-white p-4 shadow-xl md:w-80"
         action={signup}
       >
-        <h2 className="text-2xl font-bold">SignUp</h2>
-        <hr className="w-full" />
+        <div className="flex flex-col items-center justify-end">
+          <p className="font-serif text-2xl">Sign Up For</p>
+          <h1 className="font-sans text-4xl font-semibold text-black/80">
+            PROTEK 2024
+          </h1>
+          <p className="text-sm text-black/50">Powered by μLearn PRC</p>
+          <hr className="mt-3 w-3/4 bg-black/10" />
+        </div>
         <label htmlFor="name">
-          Name
-          <input type="string" name="name" id="name" />
+          <h1 className="w-max rounded-xl bg-rose-200 p-2 font-serif text-sm">
+            Name
+          </h1>
+          <input type="string" name="name" id="name" placeholder="Name"/>
         </label>
 
         <label htmlFor="email">
-          Email
-          <input type="email" name="email" id="email" />
+          <h1 className="w-max rounded-xl bg-purple-300 p-2 font-serif text-sm">
+            E Mail
+          </h1>
+          <input type="email" name="email" id="email" placeholder="example@gmail.com" />
         </label>
 
         <label htmlFor="phno">
-          Phone Number
-          <input type="string" name="phno" id="phno" />
+          <h1 className="w-max rounded-xl bg-green-300 p-2 font-serif text-sm">
+            Phone Number
+          </h1>
+          <input type="string" name="phno" id="phno" placeholder="+91 462658626" />
         </label>
+        <div className="flex flex-row gap-20">
+          <h1 className="w-max rounded-xl bg-orange-300 p-2 font-serif text-sm">
+            Department
+          </h1>
+          <h1 className="w-max rounded-xl bg-blue-300 p-2 font-serif text-sm">
+            Year
+          </h1>
+        </div>
+        <div className="flex flex-row gap-5">
+          <label htmlFor="dept">
+            <input type="string" name="dept" id="dept" placeholder="eg:ME" />
+          </label>
 
-        <label htmlFor="dept">
-          Department
-          <input type="string" name="dept" id="dept" />
-        </label>
-
-        <label htmlFor="year">
-          Year
-          <input type="string" name="year" id="year" />
-        </label>
+          <label htmlFor="year">
+            <input type="string" name="year" id="year" placeholder="2024" />
+          </label>
+        </div>
 
         <label htmlFor="password">
-          Password
-          <input type="password" name="password" id="password" />
+          <h1 className="w-max rounded-xl bg-purple-300 p-2 font-serif text-sm">
+            Password
+          </h1>
+
+          <input type="password" name="password" id="password" placeholder="Password"/>
         </label>
 
         <label htmlFor="confirm">
-          Confirm Password
-          <input type="password" name="confirm" id="confirm" />
+          <h1 className="w-max rounded-xl bg-yellow-200 p-2 font-serif text-sm">
+            Confirm Password
+          </h1>
+          <input type="password" name="confirm" id="confirm" placeholder="Retype Password" />
         </label>
 
         <button
           type="submit"
-          className="rounded-[0.5rem] bg-blue-500 p-2 text-white"
+          className="rounded-[0.5rem] bg-green-500 p-2 font-serif"
         >
           Submit
         </button>
